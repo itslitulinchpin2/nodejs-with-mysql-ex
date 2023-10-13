da0885e15fc947da39cca374bbc8e776b66027ad
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-10-13 09:55:15 +0200

    CRUD - update implementation

--- main.js
+++ main.js
@@ -75,24 +75,7 @@ var app = http.createServer(function(request,response){
 
       }
     } else if(pathname === '/create'){
-      // fs.readdir('./data', function(error, filelist){
-      //   var title = 'WEB - create';
-      //   var list = template.list(filelist);
-      //   var html = template.HTML(title, list, `
-      //     <form action="/create_process" method="post">
-      //       <p><input type="text" name="title" placeholder="title"></p>
-      //       <p>
-      //         <textarea name="description" placeholder="description"></textarea>
-      //       </p>
-      //       <p>
-      //         <input type="submit">
-      //       </p>
-      //     </form>
-      //   `, '');
-      //   response.writeHead(200);
-      //   response.end(html);
-      // });
-
+      
       db.query(`SELECT * FROM TOPIC`, function(error, topics){
           
         //쿼리문의 결과값이 topics에 담긴다.
@@ -137,30 +120,41 @@ var app = http.createServer(function(request,response){
           
       });
     } else if(pathname === '/update'){
-      fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
+
+      db.query(`SELECT * FROM topic`,function(error,topics){
+        if(error){
+          throw error;
+        } 
+        console.log("첫번째 토픽: ", topics)
+        db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
+          if (error2){
+            throw error2;
+          }
+          console.log("두번째 토픽: ", topic)
+        var list = template.list(topics);
+        var html = template.HTML(topic[0].title, list,
             `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
+              <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${topic[0].id}">
+                <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                <p>
+                  <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>
+              `,
+              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+
+        )
+        response.writeHead(200);
+        response.end(html);
+
       });
+    });
+
+
     } else if(pathname === '/update_process'){
       var body = '';
       request.on('data', function(data){
@@ -169,14 +163,19 @@ var app = http.createServer(function(request,response){
       request.on('end', function(){
           var post = qs.parse(body);
           var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
-          });
+  
+          db.query(`
+          UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?`,
+          [post.title,post.description,post.id],
+          
+          function(error,results){
+            if(error){
+              throw error;
+            }
+            response.writeHead(302,{Location:`/?id=${post.id}`})
+            response.end();
+          }
+          )
       });
     } else if(pathname === '/delete_process'){
       var body = '';
