eab9190f4fd5eb3085a47e3e3c36d5ec548bd7e4
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-10-15 15:12:30 +0200

    filterng by sanitize-HTML for security

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 var http = require('http');
 var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var db = require('./lib/db')
 var topic=require('./lib/topic')
 var author=require('./lib/author')
+
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
